3c88b8c29245fd1784b9769beea42e222271b62b
Plant Lady <codeplantsbylouie@gmail.com>
2019-03-02 20:39:34 -0500

    html change and added more to js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,20 +13,41 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // BUTTON FOR ADDING EMPLOYEES
-
+$("#addTrain-btn"),on("click", function(event) {
+  event.preventDefault();
+})
   // GRAB USER INPUT
-
+  var trainName = $("#train-name-input").val().trim();
+  var destination = $("#destination-input").val().trim();
+  var trainTime = moment($("#time-input").val().trim(), "HH:MM:SS")
+  var frequency = $("#frequency-input").val().trim();
   // CREATE LOCAL "TEMPORARY" OBJECT FOR HOLDING TRAIN DATA
+  var newTrain = {
+    name: trainName,
+    place: destination,
+    time: trainTime,
+    freq: frequency
+  };
 
   // UPLOAD TRAIN DATA TO THE DATABASE
-
+  database.ref().push(newTrain);
   // LOGS EVERYTHING TO CONSOLE
+  console.log(newTrain.name);
+  console.log(newTrain.place);
+  console.log(newTrain.time);
+  console.log(newTrain.freq);
 
+  alert("train successfully added");
   // CLEAR ALL OF THE TEXT BOXES
-
-
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#time-input").val("");
+  $("#frequency-input").val("");
 // CREATE FIREBASE EVENT FOR ADDING TRAIN TO THE DATABASE AND A ROW IN THE HTML WHEN A USER ADDS AN ENTRY
-
+database.ref().on("child_added", function(chilcSnapshot) {
+  console.log(childSnapshot.val());
+  
+})
   // STORE EVERYTHING INTO A VARIABLE
 
   // TRAIN INFO
